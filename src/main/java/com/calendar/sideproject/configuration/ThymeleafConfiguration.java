0dc970d8baf72a67289f2c922bf85ad3f99e456a
Mpho Mahase <mpho.mahase@entelect.co.za>
2021-05-26 22:55:38 +0200

    Added initial code infrastructure for the online java calendar

---
+++ src/main/java/com/calendar/sideproject/configuration/ThymeleafConfiguration.java
@@ -0,0 +1,30 @@
+package com.calendar.sideproject.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+//@Configuration
+public class ThymeleafConfiguration {
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        var templateResolver = new SpringResourceTemplateResolver();
+//        templateResolver.setApplicationContext(this.applicationContext);
+        templateResolver.setPrefix("src/main/resources/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCacheable(true);
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        var templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
+    }
+}
